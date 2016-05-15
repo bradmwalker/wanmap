1397553751a3d174f845cdfa404b748dad6fb997
Brad Walker <brad@bradmwalker.com>
2016-05-15 11:45:32 -0400

    Add get_session_factory and get_tm_session

--- wanmap/schema.py
+++ wanmap/schema.py
@@ -273,6 +273,39 @@ def get_engine(settings):
         return Engine
 
 
+def get_session_factory(connectable):
+    factory = sessionmaker()
+    factory.configure(bind=connectable)
+    return factory
+
+
+def get_tm_session(session_factory, transaction_manager):
+    """
+    Get a ``sqlalchemy.orm.Session`` instance backed by a transaction.
+
+    This function will hook the session to the transaction manager which
+    will take care of committing any changes.
+
+    - When using pyramid_tm it will automatically be committed or aborted
+      depending on whether an exception is raised.
+
+    - When using scripts you should wrap the session in a manager yourself.
+      For example::
+
+          import transaction
+
+          engine = get_engine(settings)
+          session_factory = get_session_factory(engine)
+          with transaction.manager:
+              dbsession = get_tm_session(session_factory, transaction.manager)
+
+    """
+    dbsession = session_factory()
+    zope.sqlalchemy.register(
+        dbsession, transaction_manager=transaction_manager)
+    return dbsession
+
+
 # run configure_mappers after defining all of the models to ensure
 # all relationships can be setup
 configure_mappers()
