43a48af8c8499a91d593e13993ce5f76b8332583
Brad Walker <brad@bradmwalker.com>
2016-10-02 20:48:11 -0600

    Add dmzscanner for public address block

--- testnet.py
+++ testnet.py
@@ -22,6 +22,7 @@ INTERNAL_BROKER_URL = 'amqp://guest@10.1.0.10/'
 INTERNET_IP = '192.0.2.1'
 EXTERNAL_SCANNER_IP = '198.51.100.2'
 EXTERNAL_BROKER_URL = 'amqp://guest@192.0.2.1/'
+DMZ_BLOCK = u'203.0.113.0/24'
 
 
 def main():
@@ -60,19 +61,23 @@ def run(interactive):
 
     dc_gateway = ip_interface(u'10.1.0.1/24')
     dc_subnet = dc_gateway.network
+    dmz_gateway = ip_interface(u'203.0.113.1/24')
+    dmz_subnet = dmz_gateway.network
     branch_gateway = ip_interface(u'10.2.0.1/24')
     branch_subnet = branch_gateway.network
 
     dc_dist = net.addHost('r0', cls=LinuxRouter, ip=str(dc_gateway))
+    dmz_fw = net.addHost('dmz', cls=LinuxRouter, ip=str(dmz_gateway))
     branch_dist = net.addHost('r1', cls=LinuxRouter, ip=str(branch_gateway))
     external_scanner = net.addHost(
         'external',
         cls=ScannerNode, broker_url=EXTERNAL_BROKER_URL,
         ip='198.51.100.2/30')
 
-    switches = tuple(net.addSwitch('s{:d}'.format(n)) for n in range(2))
+    switches = tuple(net.addSwitch('s{:d}'.format(n)) for n in range(3))
     net.addLink(switches[0], dc_dist)
-    net.addLink(switches[1], branch_dist)
+    net.addLink(switches[1], dmz_fw)
+    net.addLink(switches[2], branch_dist)
 
     dc_to_branch = Link(
         dc_dist, branch_dist,
@@ -95,17 +100,34 @@ def run(interactive):
 
     dc_dist.cmd('iptables -t nat -A PREROUTING -i dc-to-external -p tcp -m tcp --dport 5672 -j DNAT --to 10.1.0.10')
 
+    dc_to_dmz = Link(
+        dc_dist, dmz_fw,
+        intfName1='dc-to-dmz', intfName2='dmz-to-dc')
+    dc_to_dmz.intf1.setIP('192.168.0.5/30')
+    dc_dist.cmd('ip route add {} via 192.168.0.6'.format(str(dmz_subnet)))
+    dc_to_dmz.intf2.setIP('192.168.0.6/30')
+    dmz_fw.setDefaultRoute('via 192.168.0.5')
+    dmz_fw.cmd('iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT')
+    dmz_fw.cmd('iptables -A FORWARD -d 10.1.0.10 -m tcp -p tcp --dport amqp --syn -j ACCEPT')
+    dmz_fw.cmd('iptables -A FORWARD -j DROP')
+
     scanner1 = net.addHost(
         'scanner1',
         cls=ScannerNode, broker_url=INTERNAL_BROKER_URL,
         ip='10.1.0.254/24', defaultRoute='via 10.1.0.1')
     net.addLink(scanner1, switches[0])
 
+    dmz_scanner = net.addHost(
+        'dmzscanner',
+        cls=ScannerNode, broker_url=INTERNAL_BROKER_URL,
+        ip='203.0.113.254/24', defaultRoute='via 203.0.113.1')
+    net.addLink(dmz_scanner, switches[1])
+
     scanner2 = net.addHost(
         'scanner2',
         cls=ScannerNode, broker_url=INTERNAL_BROKER_URL,
         ip='10.2.0.254/24', defaultRoute='via 10.2.0.1')
-    net.addLink(scanner2, switches[1])
+    net.addLink(scanner2, switches[2])
 
     console = net.addHost(
         'console', ip=CONSOLE_IP, defaultRoute='via 10.1.0.1',
