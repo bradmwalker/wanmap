d8bfc5a87f359523fc4505f874dacc940c259c67
Brad Walker <brad@bradmwalker.com>
2017-04-19 02:31:10 -0600

    Use global fake_wan_scanners fixture in Scan.create tests

--- wanmap/tasks_test.py
+++ wanmap/tasks_test.py
@@ -2,44 +2,23 @@ import logging
 
 import pytest
 
-from .schema import DeltaScan, Scanner, ScannerSubnet, SplittingScan
+from .schema import DeltaScan, SplittingScan
 
 PING_SWEEP = '-sn -PE -n'
 
 _logger = logging.getLogger(__name__)
 
 
-@pytest.fixture
-def scanners():
-    scanner_a = Scanner.create(
-        name='scanner-a', interface_address='10.0.0.2/24')
-    scanner_a.subnets.append(
-        ScannerSubnet(scanner=scanner_a, subnet='192.168.0.0/24'))
-    scanner_b = Scanner.create(
-        name='scanner-b', interface_address='10.0.1.2/24')
-    return {scanner_a, scanner_b}
-
-
-@pytest.fixture
-def persisted_scanners(dbsession, scanners):
-    dbsession.add_all(scanners)
-    dbsession.flush()
-    return scanners
-
-
-def test_splitting_scan(dbsession, persisted_scanners):
+def test_splitting_scan(dbsession, fake_wan_scanners):
     scan = SplittingScan.create(
         session=dbsession, parameters=PING_SWEEP,
         targets=('10.0.0.0/8',))
-    subscans = scan.subscans
-    scanners = {subscan.scanner for subscan in subscans}
-    assert persisted_scanners == scanners
     subscan_targets = {
         target.target
-        for subscan in subscans
+        for subscan in scan.subscans
         for target in subscan.targets
     }
-    scanner_subnets = {'10.0.0.0/24', '10.0.1.0/24'}
+    scanner_subnets = {'10.1.0.0/24', '10.2.0.0/24'}
     assert subscan_targets == scanner_subnets
 
 
@@ -57,33 +36,33 @@ def test_create_splitting_scan_errors_on_no_subnet_matches(dbsession):
             targets=('0.0.0.0/0',))
 
 
-def test_create_splitting_host_match(dbsession, persisted_scanners):
+def test_create_splitting_host_match(dbsession, fake_wan_scanners):
     scan = SplittingScan.create(
         session=dbsession, parameters=PING_SWEEP,
-        targets=('10.0.1.1',))
+        targets=('10.1.0.1',))
     subscan_targets = {
         target.target
         for subscan in scan.subscans
         for target in subscan.targets
     }
-    assert subscan_targets == {'10.0.1.1/32'}
+    assert subscan_targets == {'10.1.0.1/32'}
 
 
-def test_create_delta_scan(dbsession, persisted_scanners):
-    scanner_names = tuple(scanner.name for scanner in persisted_scanners)
+def test_create_delta_scan(dbsession, fake_wan_scanners):
+    scanner_a, scanner_b, *_ = (scanner.name for scanner in fake_wan_scanners)
     scan = DeltaScan.create(
         session=dbsession, parameters=PING_SWEEP,
-        scanner_names=scanner_names, targets=('10.0.1.1',))
+        scanner_names=(scanner_a, scanner_b,), targets=('10.1.0.1',))
     subscan_targets = {
         target.target
         for subscan in scan.subscans
         for target in subscan.targets
     }
-    assert subscan_targets == {'10.0.1.1/32'}
+    assert subscan_targets == {'10.1.0.1/32'}
 
 
-def test_create_delta_scan_errors_on_no_targets(dbsession, persisted_scanners):
-    scanner_names = tuple(scanner.name for scanner in persisted_scanners)
+def test_create_delta_scan_errors_on_no_targets(dbsession, fake_wan_scanners):
+    scanner_names = tuple(scanner.name for scanner in fake_wan_scanners)
     with pytest.raises(ValueError):
         DeltaScan.create(
             session=dbsession, parameters=PING_SWEEP,
