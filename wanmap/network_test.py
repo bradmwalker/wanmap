2ea3e0c00eddfed671b60f343d0ae987a5d3c117
Brad Walker <brad@bradmwalker.com>
2017-05-20 13:13:51 -0600

    Validate resolvability of seed router host

--- wanmap/network_test.py
+++ wanmap/network_test.py
@@ -19,6 +19,42 @@ def test_discovery_form_rejects_blank_seed_router_host(discovery_form):
     assert 'Required' in exc.value.render()
 
 
+def test_discovery_form_allows_ipv4_address(discovery_form):
+    appstruct = {
+        'seed_router_host': '10.1.0.1',
+        'username': 'wanmap', 'password': 'wanmap',
+    }
+    discovery_form.validate_pstruct(appstruct)
+
+
+def test_discovery_form_allows_ipv6_address(discovery_form):
+    appstruct = {
+        'seed_router_host': 'fd12:3456:789a:1::1',
+        'username': 'wanmap', 'password': 'wanmap',
+    }
+    discovery_form.validate_pstruct(appstruct)
+
+
+def test_discovery_form_allows_resolvable_hostname(discovery_form, fake_dns):
+    appstruct = {
+        'seed_router_host': 'r0.local',
+        'username': 'wanmap', 'password': 'wanmap',
+    }
+    discovery_form.validate_pstruct(appstruct)
+
+
+def test_discovery_form_rejects_unresolvable_hostname(
+    discovery_form, fake_dns):
+
+    appstruct = {
+        'seed_router_host': 'public.local',
+        'username': 'wanmap', 'password': 'wanmap',
+    }
+    with pytest.raises(ValidationFailure) as exc:
+        discovery_form.validate_pstruct(appstruct)
+    assert 'Required' in exc.value.render()
+
+
 def test_discovery_form_rejects_blank_username(discovery_form):
     appstruct = {
         'seed_router_host': '10.1.0.1',
