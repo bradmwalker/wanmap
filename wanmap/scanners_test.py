3fbd5be23540548cd26a45d96cff2476fa21edac
Brad Walker <brad@bradmwalker.com>
2017-05-28 12:17:37 -0600

    Simplify scanners_test

--- wanmap/scanners_test.py
+++ wanmap/scanners_test.py
@@ -1,23 +1,8 @@
 import logging
 
-from pyramid.httpexceptions import HTTPNotFound
-import pytest
-
 from .scanners import Scanner, show_scanners
 
 logger = logging.getLogger(__name__)
-
-
-@pytest.fixture
-def scanner():
-    return Scanner.create('test', '10.0.0.2/24')
-
-
-@pytest.fixture
-def persisted_scanner(dbsession, scanner):
-    dbsession.add(scanner)
-    dbsession.flush()
-    return scanner
 
 
 def test_show_scanners_without_scanners(view_request):
@@ -25,7 +10,7 @@ def test_show_scanners_without_scanners(view_request):
     assert response['scanners'] == []
 
 
-def test_show_scanners_with_scanners(view_request, persisted_scanner):
+def test_show_scanners_with_scanners(view_request, fake_wan_scanners):
     response = show_scanners(view_request)
     assert (
         response['scanners'] and
