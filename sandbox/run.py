457acd73d3b20b7a53369ee6dca50c86cd5ff79f
Brad Walker <brad@bradmwalker.com>
2020-05-09 22:00:28 -0600

    Introduce Router wrapper class

--- sandbox/run.py
+++ sandbox/run.py
@@ -18,17 +18,29 @@ def main():
 
     network = bridge_xml('vw001')
     network = conn.networkCreateXML(network)
-    guest = create_router('dc')
-    guest = conn.createXML(guest, 0)
-    upload_vyos_config('dc')
+    dc = Router('dc')
+    dc.start(conn)
+    dc.configure()
     input()
-    guest.destroy()
+    dc.stop()
     network.destroy()
 
 
-def create_router(name: str):
-    guest = f'''<domain type='kvm'>
-  <name>{name}</name>
+class Router:
+
+    def __init__(self, name: str):
+        self._name = name
+
+    def start(self, hypervisor: libvirt.virConnect):
+        self._guest = hypervisor.createXML(self.xml, 0)
+
+    def stop(self):
+        self._guest.destroy()
+
+    @property
+    def xml(self):
+        return f'''<domain type='kvm'>
+  <name>{self._name}</name>
   <memory>524288</memory>
   <vcpu>1</vcpu>
   <os>
@@ -54,7 +66,34 @@ def create_router(name: str):
   </devices>
 </domain>
 '''
-    return guest
+
+    def configure(self):
+        console = pexpect.spawn(
+            f'virsh -c qemu:///system console {self._name}',
+            # Allow bootup time
+            timeout=60)
+        console.sendline('')
+        console.expect('.+ login: ')
+        console.sendline('vyos')
+        console.expect('Password: ')
+        console.sendline('vyos')
+        console.expect(r'vyos@.+:~\$ ')
+        console.sendline('cat > vyos.config')
+        with open(f'{self._name}.config', 'rt') as file_:
+            for line in file_.readlines():
+                console.send(line)
+        console.send('')
+        console.sendline('configure')
+        console.expect('vyos@.+# ')
+        console.sendline('load vyos.config')
+        console.expect('vyos@.+# ')
+        console.sendline('commit')
+        console.expect('vyos@.+# ')
+        console.sendline('exit')
+        console.expect(r'vyos@.+:~\$ ')
+        console.sendline('exit')
+        console.sendline('exit')
+        console.close()
 
 
 def bridge_xml(name: str) -> str:
@@ -63,33 +102,6 @@ def bridge_xml(name: str) -> str:
   <bridge name="{name}"/>
 </network>
 '''
-
-
-def upload_vyos_config(name: str):
-    # Timeout 60 for bootup
-    console = pexpect.spawn(f'virsh -c qemu:///system console {name}', timeout=60)
-    console.sendline('')
-    console.expect('.+ login: ')
-    console.sendline('vyos')
-    console.expect('Password: ')
-    console.sendline('vyos')
-    console.expect(r'vyos@.+:~\$ ')
-    console.sendline('cat > vyos.config')
-    with open(f'{name}.config', 'rt') as file_:
-        for line in file_.readlines():
-            console.send(line)
-    console.send('')
-    console.sendline('configure')
-    console.expect('vyos@.+# ')
-    console.sendline('load vyos.config')
-    console.expect('vyos@.+# ')
-    console.sendline('commit')
-    console.expect('vyos@.+# ')
-    console.sendline('exit')
-    console.expect(r'vyos@.+:~\$ ')
-    console.sendline('exit')
-    console.sendline('exit')
-    console.close()
 
 
 if __name__ == '__main__':
