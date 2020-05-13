270a31ab2c671c7a4e756288776f650b57b2cb64
Brad Walker <brad@bradmwalker.com>
2020-05-13 12:09:16 -0600

    Add DMZ router with dmzscanner Scanner

---
+++ sandbox/dmzscanner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+broker_ip_address=10.1.0.10
+celery_path=/opt/wanmap/bin/celery
+
+# Establish connectivity before starting Celery to ensure celeryd_after_setup
+# event messages persist_scanner task.
+until nping --tcp -p 6379 -c 1 $broker_ip_address | grep ' SA '; do
+    sleep .5
+done
+sudo -u wanmap $celery_path worker -A wanmap.tasks -b redis://$broker_ip_address -l INFO -n scanner@$(hostname -s) -X console
