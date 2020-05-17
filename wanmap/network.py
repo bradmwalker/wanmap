4dabd2cb9398e9604f8aef590317249d9233945d
Brad Walker <brad@bradmwalker.com>
2020-05-17 11:04:02 -0600

    Update network discovery to use NAPALM
    Only VyOS routers are currently supported. LLDP-based network discovery now
    requires valid resolvable hostnames.

--- wanmap/network.py
+++ wanmap/network.py
@@ -8,10 +8,10 @@ import arrow
 import colander
 from deform import Form, ValidationFailure
 from deform.widget import PasswordWidget
-import paramiko
+from napalm import get_network_driver
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
-from sqlalchemy import Column, DateTime, ForeignKey
+from sqlalchemy import Column, DateTime, ForeignKey, String
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import joinedload, relationship
 import transaction
@@ -81,20 +81,19 @@ class Router(Persistable):
 
     __tablename__ = 'routers'
 
-    # TODO: Determine a better key.
-    id = Column(postgresql.UUID(as_uuid=True), primary_key=True)
+    hostname = Column(String, primary_key=True)
     last_collected_at = Column(DateTime(timezone=True), nullable=False)
 
     _interfaces = relationship(
         'RouterInterface', cascade='all, delete-orphan', backref='router')
 
     @classmethod
-    def create(cls, id_, interfaces):
+    def create(cls, hostname, interfaces):
         interfaces = [
             RouterInterface(address=interface) for interface in interfaces
         ]
         return cls(
-            id=id_,
+            hostname=hostname,
             last_collected_at=arrow.now().datetime,
             _interfaces=interfaces,
         )
@@ -120,9 +119,8 @@ class RouterInterface(Persistable):
 
     __tablename__ = 'router_interfaces'
 
-    router_id = Column(
-        postgresql.UUID(as_uuid=True), ForeignKey('routers.id'),
-        primary_key=True)
+    router_hostname = Column(
+        String, ForeignKey('routers.hostname'), primary_key=True)
     address = Column(postgresql.INET, primary_key=True)
 
 
@@ -141,70 +139,51 @@ class DiscoveryValidator(colander.Schema):
         return Form(schema, formid='discover-network', buttons=('submit',))
 
 
-def discover_network(seed_router_address, credentials):
+def discover_network(seed_router_hostname, credentials):
     """Traverses the network using BFS and collects router information."""
 
-    to_visit = deque((seed_router_address,))
+    to_visit = deque((seed_router_hostname,))
     routers = []
-    known_addresses = set()
+    visited = set()
     while to_visit:
         visiting = to_visit.popleft()
-        if visiting in known_addresses:
+        if visiting in visited:
             continue
-        router = get_router(visiting, credentials)
+        visited.add(visiting)
+        router, neighbors = get_router(visiting, credentials)
         if router:
             routers.append(router)
-            known_addresses |= {
-                interface.ip for interface in router.interfaces
-            }
-            to_visit.extend(neighbor_addresses(router.interfaces))
+            to_visit.extend(
+                neighbor for neighbor in neighbors if neighbor not in visited)
     return routers
 
 
-def get_router(router_address, credentials):
-    """Visits the router and retrieves its UUID and interfaces."""
+def get_router(hostname, credentials):
+    """Collects router information and returns its LLDP neighbors."""
 
-    with paramiko.SSHClient() as ssh:
-        logger.info('Collecting router information at %s', router_address)
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        try:
-            ssh.connect(
-                str(router_address),
-                username=credentials[0], password=credentials[1])
-        except paramiko.ssh_exception.NoValidConnectionsError:
-            return None
-        else:
-            router = Router.create(
-                id_=_get_router_id(ssh),
-                interfaces=_get_router_interfaces(ssh))
-            logger.info(
-                'Collected router %s with %d interfaces.',
-                router.id, len(router.interfaces))
-            return router
+    # TODO: Fingerprint router before initializing proper driver
+    driver = get_network_driver('vyos')
+    with driver(hostname, *credentials) as device:
+        logger.info('Collecting router information for %s', hostname)
+        router = Router.create(
+            hostname=hostname,
+            interfaces=_get_router_interfaces(device))
+        logger.info(
+            'Collected router %s with %d interfaces',
+            router.hostname, len(router.interfaces))
+        neighbors = _get_neighbors(device)
+        return router, neighbors
 
 
-def _get_router_id(ssh):
-    _, stdout, _ = ssh.exec_command('/usr/bin/cat /etc/ssh/uuid')
-    output = stdout.read().decode()
-    return UUID(output.strip())
-
-
-def _get_router_interfaces(ssh):
-    interfaces_output = _get_interfaces_output(ssh)
-    return _parse_interfaces(interfaces_output)
-
-
-def _get_interfaces_output(ssh):
-    _, stdout, _ = ssh.exec_command('/usr/sbin/ip -o -f inet address')
-    return stdout.read().decode()
-
-
-def _parse_interfaces(interfaces_output):
-    interface_regex = re.compile(r'inet ([.\d/]+) ')
-    captured_interfaces = (
-        match.group(1) for match in interface_regex.finditer(interfaces_output)
-    )
-    interfaces = map(ip_interface, captured_interfaces)
+def _get_router_interfaces(device):
+    ips = device.get_interfaces_ip()
+    interfaces = []
+    for interface, protocols in ips.items():
+        for protocol, addresses in protocols.items():
+            for address, prefix_len in addresses.items():
+                address = ip_interface(
+                    f"{address}/{prefix_len['prefix_length']}")
+                interfaces.append(address)
     return frozenset(
         interface for interface in interfaces
         if not interface.is_link_local if not interface.is_loopback
@@ -212,9 +191,10 @@ def _parse_interfaces(interfaces_output):
     )
 
 
-def neighbor_addresses(interfaces):
-    glue_net_interfaces = {
-        interface for interface in interfaces
-        if interface.network.prefixlen == 30    # TODO: Handle /31, /32
-    }
-    return frozenset(map(opposite_address, glue_net_interfaces))
+def _get_neighbors(device):
+    lldp_neighbors = device.get_lldp_neighbors()
+    neighbors = set()
+    for interface, neighbor_entries in lldp_neighbors.items():
+        for entry in neighbor_entries:
+            neighbors.add(entry['hostname'])
+    return neighbors
